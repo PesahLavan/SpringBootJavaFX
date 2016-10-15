e643827751322f9d7386d17a7f2678c709215e65
Beljaev_Pavel <beljaev.pavel.kr@gmail.com>
2016-10-15 09:41:06 +0200

    init project

---
+++ src/main/java/com/pesahlavan/training/javafx/AdrBookSpringBootJfxApplication.java
@@ -0,0 +1,12 @@
+package com.pesahlavan.training.javafx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdrBookSpringBootJfxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdrBookSpringBootJfxApplication.class, args);
+	}
+}
